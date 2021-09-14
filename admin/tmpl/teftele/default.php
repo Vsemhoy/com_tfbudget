7d7953980d22c39107af687a1bd72de3a24fdbb7
Vsemhoy <77360455+Vsemhoy@users.noreply.github.com>
2021-09-14 16:37:46 +0300

    Step 4 - Localization ini
    Localization added

--- admin/tmpl/teftele/default.php
+++ admin/tmpl/teftele/default.php
@@ -8,7 +8,9 @@
  * @license     GNU General Public License version 3; see LICENSE
  */
 
+use Joomla\CMS\Language\Text;
+
  // No direct access to this file
 defined('_JEXEC') or die('Restricted Access');
 ?>
-<h2>Teftele-World!</h2>
+<h2><?= Text::_('COM_TEFTELEBUDGET_MSG_HELLO_TEFTELE') ?></h2>
